79c71d314bbfe5db5b937ab7f57270853074720c
Natalia Burguillo Martin <natalia.burguillo@alumnos.upm.es>
2025-02-10 18:29:39 +0100

    Initial Project in Spring Boot with Thymeleaf

---
+++ src/main/java/com/isst/ISST_Grupo25_Casas/IsstGrupo25CasasApplication.java
@@ -0,0 +1,13 @@
+package com.isst.ISST_Grupo25_Casas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsstGrupo25CasasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsstGrupo25CasasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isst/ISST_Grupo25_Casas/IsstGrupo25CasasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isst.ISST_Grupo25_Casas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsstGrupo25CasasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
