70a7fb24282cf2c58f9bc0dc5de9b5f1963197b5
Rodrigo Tavares de Pina Simões <rod.tpsim@gmail.com>
2025-04-07 20:11:36 +0200

    Configuración del Tema Oscuro y Tema Claro

---
+++ src/main/java/com/isst/ISST_Grupo25_Casas/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.isst.ISST_Grupo25_Casas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ThemeInterceptor());
+    }
+}
